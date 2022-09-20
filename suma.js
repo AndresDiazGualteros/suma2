66e232fb3df6b4e9a028801385ecc3b818e8b94c
AndresDiazGualteros <mac@macs-iMac.local>
2022-09-20 18:58:39 -0500

    mi suma

--- suma.js
+++ suma.js
@@ -1,2 +1,10 @@
 let numero1, numero2;
 let suma;
+
+numero1 = +prompt("ingrese un numero para sumar")
+numero2 = +prompt("ingrese un numero para sumar")
+
+suma = numero1+numero2
+
+alert(`la suma de los dos numeros es de ${suma}`)
+
